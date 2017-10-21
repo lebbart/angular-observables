e64a3b97f1d1de856ed859683278db5e76acbe42
blebedenko <blebedenko@millhouse.com>
2017-10-21 17:50:44 +0300

    [A4B] Start creating custom observables.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,35 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const someNewObs = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('This does not work.');
+        observer.complete();
+      }, 5000);
+    });
+
+    someNewObs.subscribe(
+      (data: string) => {
+        console.log(data);
+      }, (error) => {
+        console.log(error);
+      }, () => {
+        console.log('completed');
+      }
+    );
   }
 
 }
